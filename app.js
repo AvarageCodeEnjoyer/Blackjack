58e0884d3524eace2786c34ae211d2e22ce7ac37
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-02-02 18:26:11 -0700

    Fixed hit button and addValue functions

--- app.js
+++ app.js
@@ -1,82 +1,97 @@
 const hit = document.getElementById('hit')
 const stand = document.getElementById('stand')
 const double = document.getElementById('double')
-const card1 = document.getElementById('card1')
-const card2 = document.getElementById('card2')
-const dealer1 = document.getElementById('dealer1')
-const dealer2 = document.getElementById('dealer2')
 const start = document.getElementById('start')
-const playerValue = document.getElementById('playerValue')
-const dealerValue = document.getElementById('dealerValue')
-let deckId;
+const playerPoints = document.getElementById('playerPoints')
+const dealerPoints = document.getElementById('dealerPoints')
+const playerCards = document.getElementById('playerCards')
+const dealerCards = document.getElementById('dealerCards')
+let startChecked = false
+let playArea
+let deckId
 
 window.onload = () => {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6")
     .then(Response => Response.json())
     .then(data => {
-      // console.log(data.deck_id)
       deckId = data.deck_id
     })
 }
 
 start.addEventListener("click", e =>{
-  fetch(url(deckId))
+  reset()
+  console.log(playerPoints.style.color)
+  let total = 0;
+  startChecked = true
+  fetch(drawCard(deckId, 4))
   .then(Response => Response.json())
   .then(data =>{
-    card1.src = data.cards[0].image
-    card2.src = data.cards[1].image
-    dealer1.src = data.cards[2].image
-    dealer2.src = data.cards[3].image
-    playerValue.innerHTML = addValue(data.cards[0].value, data.cards[1].value)
-    dealerValue.innerHTML = addValue(data.cards[2].value, data.cards[3].value)
-    console.log(data.cards[0])
+    for(i = 0; i <= 1; i++){addCards(data, playerCards, i, playerPoints)}
+    for(i = 2; i <= 3; i++){addCards(data, dealerCards, i, dealerPoints)}
+    playerPoints.innerHTML = addValue(data.cards[0].value, total) + addValue(data.cards[1].value, total)
+    dealerPoints.innerHTML = addValue(data.cards[2].value, total) + addValue(data.cards[3].value, total)
   })
 })
 
-function addValue(value1, value2){
-  let total = 0;
-
-  if(value1 === "ACE"){total += 11}
-  else if(value1 === "KING" || value1 === "QUEEN" || value1 === "JACK"){total += 10}
-  else{total += parseInt(value1)}
-
-  if(value2 === "ACE"){total += 11}
-  else if(value2 === "KING" || value2 === "QUEEN" || value2 === "JACK"){total += 10}
-  else{total += parseInt(value2)}
-
-  return total
-}
 
 
 hit.addEventListener("click", e => {
+  if(!startChecked) return
   e.preventDefault()
-  fetch(url(deckId))
+  fetch(drawCard(deckId, 1))
   .then(Response => Response.json())
   .then(data =>{
-    card2.src = data.cards[0].image
-    console.log(data.cards[0])
+    addCards(data, playerCards, 0, playerPoints)
   })
 })
 
-double.addEventListener("click", e => {
+/* double.addEventListener("click", e => {
+  if(!startChecked) return
   e.preventDefault()
-  fetch(url(deckId))
+  fetch(drawCard(deckId))
   .then(Response => Response.json())
   .then(data =>{
     console.log(data)
   })
-})
+}) */
 
 stand.addEventListener("click", e => {
+  if(!startChecked) return
   e.preventDefault()
-  fetch(url(deckId))
-  .then(Response => Response.json())
-  .then(data =>{
-    console.log(data)
-  })
+  // if()
 })
 
+/* -------------------------------- Functions ------------------------------- */
 
-function url(deckId){
-  return `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`
+function drawCard(deckId, i){
+  return `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${i}`
+}
+
+function addCards(data, playArea, i, cardPoints){
+  let createCard = document.createElement('img')
+  createCard.src = data.cards[i].image
+  playArea.appendChild(createCard)
+  cardPoints.innerHTML = addValue(data.cards[i].value, parseInt(cardPoints.innerHTML))
+  if(parseInt(cardPoints.innerHTML) > 21){
+    cardPoints.style.color = "red"
+    console.log(playerPoints.innerHTML)
+    startChecked = false
+  }
+  return
+}
+
+function addValue(value, total){
+  if(value === "ACE"){total += 11}
+  else if(value === "KING" || value === "QUEEN" || value === "JACK"){total += 10}
+  else{total += parseInt(value)}
+  return total
+}
+
+function reset(){
+  playerPoints.style.color = "black"
+  dealerPoints.style.color = "black"
+  playerPoints.innerHTML = ""
+  dealerPoints.innerHTML = ""
+  playerCards.innerHTML = ""
+  dealerCards.innerHTML = ""
 }
