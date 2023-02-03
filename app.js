8ab90e096425d012586ae0efb380fbcf3cf9aaf1
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-02-02 22:55:14 -0700

    Added win logic, reset(), dealerDraw() hand arrays

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const dealerPoints = document.getElementById('dealerPoints')
 const playerCards = document.getElementById('playerCards')
 const dealerCards = document.getElementById('dealerCards')
 let startChecked = false
+let playerHand = []
+let dealerHand = []
 let playArea
 let deckId
+
+/* ------- Shuffle 6 decks of cards and set the "deckId" to that deck ------- */
 
 window.onload = () => {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6")
@@ -18,47 +22,76 @@ window.onload = () => {
     })
 }
 
-start.addEventListener("click", e =>{
+/* --------- Add event listener to start button to set initial state --------- */
+
+start.addEventListener("click", e => {
   reset()
-  console.log(playerPoints.style.color)
-  let total = 0;
+  let currentValue = 0;
   startChecked = true
   fetch(drawCard(deckId, 4))
   .then(Response => Response.json())
-  .then(data =>{
-    for(i = 0; i <= 1; i++){addCards(data, playerCards, i, playerPoints)}
-    for(i = 2; i <= 3; i++){addCards(data, dealerCards, i, dealerPoints)}
-    playerPoints.innerHTML = addValue(data.cards[0].value, total) + addValue(data.cards[1].value, total)
-    dealerPoints.innerHTML = addValue(data.cards[2].value, total) + addValue(data.cards[3].value, total)
+  .then(data => {
+    for(i = 0; i <= 1; i++){
+      addCards(data, playerCards, i, playerPoints) 
+      playerHand.push(data.cards[i])
+    }
+    for(i = 2; i <= 3; i++){
+      addCards(data, dealerCards, i, dealerPoints)
+      dealerHand.push(data.cards[i])
+    }
+    playerPoints.innerHTML = addValue(data.cards[0].value, currentValue) + addValue(data.cards[1].value, currentValue)
+    dealerPoints.innerHTML = addValue(data.cards[2].value, currentValue) + addValue(data.cards[3].value, currentValue)
+    if(parseInt(playerPoints.innerHTML) === 22){
+      playerPoints.innerHTML - 10
+    }
+    if(parseInt(playerPoints.innerHTML) === 21){
+      console.log("BLACKJACK!")
+    }
+    // console.log(playerPoints.innerHTML)
   })
 })
 
 
+/* ------- Event listener to "hit" button to add cards to playerHand ------- */
 
 hit.addEventListener("click", e => {
   if(!startChecked) return
   e.preventDefault()
   fetch(drawCard(deckId, 1))
   .then(Response => Response.json())
-  .then(data =>{
+  .then(data => {
+    console.log(data.cards[0])
+    playerHand.push(data.cards[0])
     addCards(data, playerCards, 0, playerPoints)
   })
+  if(parseInt(playerHand.innerHTML) === 21){
+    stand()
+  }
 })
+
+/* ------ Event listener for "double" button to double bet and add card ----- */
 
 /* double.addEventListener("click", e => {
   if(!startChecked) return
   e.preventDefault()
   fetch(drawCard(deckId))
   .then(Response => Response.json())
-  .then(data =>{
+  .then(data => {
     console.log(data)
   })
 }) */
 
+
+/* ------------ Button for "stand", controls logic of win - loss ------------ */
+
 stand.addEventListener("click", e => {
-  if(!startChecked) return
   e.preventDefault()
-  // if()
+  if(!startChecked) return
+  if(parseInt(dealerPoints.innerHTML) <= 16){
+    dealerHand.push()
+    dealerDraw()
+  }
+  else{return}
 })
 
 /* -------------------------------- Functions ------------------------------- */
@@ -74,17 +107,20 @@ function addCards(data, playArea, i, cardPoints){
   cardPoints.innerHTML = addValue(data.cards[i].value, parseInt(cardPoints.innerHTML))
   if(parseInt(cardPoints.innerHTML) > 21){
     cardPoints.style.color = "red"
-    console.log(playerPoints.innerHTML)
     startChecked = false
+    console.log("Bust")
   }
-  return
+  
 }
 
-function addValue(value, total){
-  if(value === "ACE"){total += 11}
-  else if(value === "KING" || value === "QUEEN" || value === "JACK"){total += 10}
-  else{total += parseInt(value)}
-  return total
+function addValue(value, currentValue){
+  if(value === "ACE"){
+    if(currentValue <= 10){currentValue += 11} 
+    else{currentValue += 1}
+  }
+  else if(value === "KING" || value === "QUEEN" || value === "JACK"){currentValue += 10}
+  else{currentValue += parseInt(value)}
+  return currentValue
 }
 
 function reset(){
@@ -94,4 +130,19 @@ function reset(){
   dealerPoints.innerHTML = ""
   playerCards.innerHTML = ""
   dealerCards.innerHTML = ""
+  playerHand.length = 0
+  dealerHand.length = 0
+  console.clear()
 }
+
+function dealerDraw(){
+  fetch(drawCard(deckId, 1))
+  .then(Response => Response.json())
+  .then(data => {
+    addCards(data, dealerCards, 0, dealerPoints)
+    if(parseInt(dealerPoints.innerHTML) >= parseInt(playerPoints.innerHTML) && parseInt(dealerPoints.innerHTML) <= 21){console.log("loss")}
+
+    else{console.log("win")}
+  })
+}
+
